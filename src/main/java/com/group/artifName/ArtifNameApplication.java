0a43b4028ffdc1cb1b3bc231dcbc84c7ce1ed5f1
Alexis <alexis.castillo@rseguridad.com>
2026-06-16 16:55:51 -0600

    first commit

---
+++ src/main/java/com/group/artifName/ArtifNameApplication.java
@@ -0,0 +1,13 @@
+package com.group.artifName;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtifNameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtifNameApplication.class, args);
+	}
+
+}
