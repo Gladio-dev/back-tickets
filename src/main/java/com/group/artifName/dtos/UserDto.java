e437337ef4185fe352fb1e2d63229cdae7a19b04
alexis castillo <alexis.castillo@rseguridad.com>
2026-07-07 19:59:14 -0600

    fix dto name

--- src/main/java/com/group/artifName/dtos/UserDto.java
+++ src/main/java/com/group/artifName/dtos/UserDto.java
@@ -1,7 +1,6 @@
 package com.group.artifName.dtos;
 
 import com.group.artifName.entities.Role;
-import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
